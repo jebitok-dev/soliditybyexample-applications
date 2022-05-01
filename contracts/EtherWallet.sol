86c1072a2494b44574b6c21834d1a35c32f6b960
Sharon Jebitok <sharonkosgei4@gmail.com>
2022-05-01 21:27:54 +0300

    Add Eth Wallet App

---
+++ contracts/EtherWallet.sol
@@ -0,0 +1,20 @@
+// SPDX-Licebse-Identifier: MIT
+
+contract EtherWallet {
+    address payable public owner;
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    receive() external payable {}
+
+    function withdraw(uint _amount) external {
+        require(msg.sender == owner, "caller is not owner");
+        payable(msg.sender).transfer(_amount);
+    }
+
+    function getBalance() external view returns(uint) {
+        return address(this).balance;
+    }
+}
